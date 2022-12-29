3b8e62b214dc559ea1a9c61cb1342e10825e69b7
git-leon <xleonhunter@gmail.com>
2022-12-29 18:05:01 -0500

    removed solutions

--- js/src/StringManipulation.js
+++ js/src/StringManipulation.js
@@ -5,53 +5,51 @@ class StringManipulation {
 
   // Define a method that returns the string in all uppercase letters
   toUpperCase() {
-    return this.str.toUpperCase();
+    return null;
   }
 
   // Define a method that returns the string in all lowercase letters
   toLowerCase() {
-    return this.str.toLowerCase();
+    return null;
   }
 
   // Define a method that returns the string with the first letter capitalized
   capitalizeFirstLetter() {
-    return this.str.charAt(0).toUpperCase() + this.str.slice(1);
+    return null;
   }
 
   // Define a method that returns the string with the first letter of each word capitalized
   capitalizeWords() {
-    return this.str.replace(/\b[a-z]/g, function(letter) {
-      return letter.toUpperCase();
-    });
+    return null;
   }
 
   // Define a method that returns an array of the words in the string
   stringToArray() {
-    return this.str.split(" ");
+    return null;
   }
 
   // Define a method that returns a string with the words in the provided array joined together
   arrayToString(arr) {
-    return arr.join(" ");
+    return null;
   }
 
   // Define a method that returns the length of the string
   getStringLength() {
-    return this.str.length;
+    return null;
   }
 
   // Define a method that returns the string repeated the provided number of times
   repeatString(num) {
-    return this.str.repeat(num);
+    return null;
   }
 
   // Define a method that returns the string with all vowels removed
   removeVowels() {
-    return this.str.replace(/[aeiou]/gi, "");
+    return null;
   }
 
   // Define a method that returns the string with all punctuation removed
   removePunctuation() {
-    return this.str.replace(/[^\w\s]/gi, "");
+    return null;
   }
 }
