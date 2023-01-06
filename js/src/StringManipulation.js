ad593c87ac11d910bf11f867733aff3466fb398c
Brent <BrentC0328@gmail.com>
2023-01-06 15:04:46 -0500

    2 methods remaining to complete

--- js/src/StringManipulation.js
+++ js/src/StringManipulation.js
@@ -5,17 +5,26 @@ class StringManipulation {
 
   // Define a method that returns the string in all uppercase letters
   toUpperCase() {
-    return null;
+  let upperCaseStr = this.str.toUpperCase();
+  return upperCaseStr;
   }
 
   // Define a method that returns the string in all lowercase letters
   toLowerCase() {
-    return null;
+  let lowerCaseStr = this.str.toLowerCase();
+    return lowerCaseStr;
   }
 
   // Define a method that returns the string with the first letter capitalized
   capitalizeFirstLetter() {
-    return null;
+
+  let capitalLetter =
+  this.str.charAt(0).
+  toUpperCase();
+
+  let restOfTheString = this.str.slice(1).toLowerCase();
+
+    return capitalLetter + restOfTheString;
   }
 
   // Define a method that returns the string with the first letter of each word capitalized
@@ -25,31 +34,39 @@ class StringManipulation {
 
   // Define a method that returns an array of the words in the string
   stringToArray() {
-    return null;
+    let stringArray = this.str.split(' ');
+    return stringArray;
   }
 
   // Define a method that returns a string with the words in the provided array joined together
   arrayToString(arr) {
-    return null;
+    const toTheString = arr.join(', ');
+    return toTheString;
   }
 
   // Define a method that returns the length of the string
   getStringLength() {
-    return null;
+    return this.str.length;
   }
 
   // Define a method that returns the string repeated the provided number of times
   repeatString(num) {
-    return null;
+
+    let repeatedString = this.str;
+
+    for (let i = 1; i < num; i++){
+    repeatedString += this.str;}
+
+    return repeatedString; 
   }
 
   // Define a method that returns the string with all vowels removed
   removeVowels() {
-    return null;
+    return this.str.replace(/[aeiou]/gi, '');
   }
 
   // Define a method that returns the string with all punctuation removed
   removePunctuation() {
-    return null;
+    return this.str.replace(/^\w\s]/gi, '');
   }
 }
